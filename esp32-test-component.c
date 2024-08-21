0f151c241b903639723853a79bbafe4c95c243b2
Tomasz Kłak <tomasz.klak@nordsec.com>
2024-08-21 13:27:22 +0200

    Add logging

--- esp32-test-component.c
+++ esp32-test-component.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include "esp_log.h"
 #include "esp32-test-component.h"
+
+static const char* TAG = "esp32-test-component";
 
 void hello_world(void)
 {
-
+  ESP_LOGI(TAG, "Hello from the test component!");
 }
